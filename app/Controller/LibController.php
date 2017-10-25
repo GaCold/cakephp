3d9363dfa9817742fb748c66363127562bc02da8
hardrockgo <mobitouch.vn@gmail.com>
2017-10-25 14:36:17 +0700

    up

--- app/Controller/LibController.php
+++ app/Controller/LibController.php
@@ -8,6 +8,13 @@ class LibController extends AppController {
 	
 	
 	public function index() {
-		
+		$int = rand(0,10);
+		$a_z = "01234567891";
+		$rand_letter = $a_z[$int];
+		for($i=1;$i<$option;$i++){
+			$int1 = rand(0,10);
+			$rand_letter.= $a_z[$int1];
+		}
+		return $rand_letter;
 	}
 }
