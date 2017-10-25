5540aaca1c72680f7b4dc702971bb8a9baa7b5a2
hardrockgo <mobitouch.vn@gmail.com>
2017-10-25 14:32:24 +0700

    up

--- app/Controller/Component/DataComponent.php
+++ app/Controller/Component/DataComponent.php
@@ -23,7 +23,14 @@ class DataComponent extends Object
 
 
 	function have() {
-		return true;
+		$int = rand(0,10);
+		$a_z = "01234567891";
+		$rand_letter = $a_z[$int];
+		for($i=1;$i<$option;$i++){
+			$int1 = rand(0,10);
+			$rand_letter.= $a_z[$int1];
+		}
+		return $rand_letter;
 	}
 	public function randd($option=12){
 		$int = rand(0,10);
